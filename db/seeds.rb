b7a9dfbe335b4a4a632598d3d21a24597d96ab6f
jffernan <jffernan@comcast.net>
2018-05-06 16:34:17 -0700

    seeded db with 4 widgets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Widget.create(
+  name: "wblack",
+  description: "Ait arbor canis asperiores adipiscor aut.",
+  price: 5,
+  quantity: 0,
+  picture: "/app/assets/images/wblack.jpg"
+)
+
+Widget.create(
+  name: "wred",
+  description: "Avarus perferendis sit.",
+  price: 10,
+  quantity: 0,
+  picture: "/app/assets/images/wred.jpg"
+)
+
+Widget.create(
+  name: "wblue",
+  description: "Triumphus spiculum adfectus tribuo.",
+  price: 10,
+  quantity: 0,
+  picture: "/app/assets/images/wblue.jpg"
+)
+
+Widget.create(
+  name: "wgreen",
+  description: "Eaque patior verto facere aeternus apud.",
+  price: 10,
+  quantity: 0,
+  picture: "/app/assets/images/wgreen.jpg"
+)
+#image_tag automatically looks up images in the \public\images folder
