05291d7a22c4ab33d5c47450239255a7b1bfc7b8
jffernan <jffernan@comcast.net>
2018-05-13 17:49:15 -0700

    fixing session helper for current_order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def current_order
-    if session[:order_id]<2
-      Order.new
-    elsif session[:order_id]
-      Order.find(session[:order_id])
+    if session[:order_id]
+       Order.find(session[:order_id])
+    else
+     Order.new
     end
   end
 
